a11fd4b2dd1c858796411d51dd64e718c8d18392
Jordan <jayala1993@outlook.com>
2015-07-03 12:50:10 -0400

    Agregar destino desde categoria

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -2,4 +2,5 @@ class Destination < ActiveRecord::Base
   extend FriendlyId
   belongs_to :category
   friendly_id :nombre, use: :slugged
+  validates_presence_of :nombre, :descripcion, :category_id
 end
