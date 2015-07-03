898ca7f5aa51d78f313c05d59398d0fa860a731b
Jordan <jayala1993@outlook.com>
2015-07-03 16:43:12 -0400

    Add google maps

--- app/helpers/landing_helper.rb
+++ app/helpers/landing_helper.rb
@@ -2,4 +2,17 @@ module LandingHelper
   def categories_list()
     Category.all
   end
+
+  def calculate_offset(count)
+    case count
+      when 1
+        'col-lg-1 col-lg-offset-4'
+      when 2
+        'col-lg-1 col-lg-offset-3'
+      when 3
+        'col-lg-1 col-lg-offset-3'
+      else
+        'col-lg-0 col-lg-offset-3'
+    end
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,8 @@ class Category < ActiveRecord::Base
   extend FriendlyId
   has_many :destinations
   friendly_id :nombre, use: :slugged
+
+  def front_destinations
+    self.destinations.where(mostrar_en_portada: true).limit(4)
+  end
 end
