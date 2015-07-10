3cf9939bb0b277cdd322d3759d480638b46c7788
Jordan <jayala1993@outlook.com>
2015-07-10 10:43:34 -0400

    Change back to projectium

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,11 +79,11 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      address:              'server1.prodominios.com',
-      port:                 465,
-      domain:               'viajemania.com.py',
-      user_name:            'adminmegusta',
-      password:             ENV['EMAIL_PASSWORD'],
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'gmail.com',
+      user_name:            'projectium15',
+      password:             'projectium123',
       authentication:       'plain',
       enable_starttls_auto: true  }
 
