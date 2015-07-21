fa658a8df8c9e792594bae6f67c39dd3461d5881
Jordan <jayala1993@outlook.com>
2015-07-21 14:11:07 -0400

    Html changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.create(nombre: 'Turismo Internacional', descripcion: 'Turismo Internacional', mostrar_en_portada: true, orden: 1)
+Category.create(nombre: 'Turismo Nacional', descripcion: 'Turismo Nacional', mostrar_en_portada: true, orden: 2)
+Category.create(nombre: 'Viaje Estudiantil', descripcion: 'Viajes Estudiantiles', mostrar_en_portada: false, orden: 3)
